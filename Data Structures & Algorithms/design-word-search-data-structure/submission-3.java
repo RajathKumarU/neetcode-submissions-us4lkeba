4ceab0606b6d9ebd38cc29ddffdbb4d72e4ace88
Rajath Kumar <rok.rajath@gmail.com>
2026-04-15 19:34:38 +0530

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.java
@@ -0,0 +1,49 @@
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<>();
+    boolean endOfWord = false;
+}
+
+class WordDictionary {
+
+    TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for(char c: word.toCharArray()) {
+            curr.children.putIfAbsent(c, new TrieNode());
+            curr = curr.children.get(c);
+        }
+
+        curr.endOfWord = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    public boolean dfs(String word, int j, TrieNode root) {
+        TrieNode curr = root;
+        
+        for(int i=j;i<word.length();i++) {
+            char c = word.charAt(i);
+            if(c == '.') {
+                for(TrieNode child: curr.children.values()) {
+                    if(child != null && dfs(word, i+1, child)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if(!curr.children.containsKey(c)) {
+                    return false;
+                } 
+                curr = curr.children.get(c);
+            }
+        }
+        return curr.endOfWord;
+    }
+}
