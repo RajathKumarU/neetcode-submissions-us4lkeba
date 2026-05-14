df1f76eba71c67fc8567f2468b6c5c3b92cbf523
Rajath Kumar <rok.rajath@gmail.com>
2026-05-14 12:16:22 +0530

    Add: minimum-difference-between-highest-and-lowest-of-k-scores - submission-0

---
+++ Data Structures & Algorithms/minimum-difference-between-highest-and-lowest-of-k-scores/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int l=0, r = k-1;
+        int res = Integer.MAX_VALUE;
+
+        while(r < nums.length) {
+            res = Math.min(res, nums[r] - nums[l]);
+            l++;
+            r++;
+        }
+        return res;
+    }
+}
