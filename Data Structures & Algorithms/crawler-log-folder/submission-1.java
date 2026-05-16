ba6b01652e19ab55beca1da63c24980915e72242
Rajath Kumar <rok.rajath@gmail.com>
2026-05-16 11:30:08 +0530

    Add: crawler-log-folder - submission-1

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stk = new Stack<>();
+
+        for(String log: logs) {
+            if (log.equals("../")) {
+                if(!stk.isEmpty())
+                    stk.pop();
+            } else if (log.equals("./")) {
+                // do nothing
+            } else {
+                stk.push(log);
+            }
+        }
+
+        return stk.size();
+    }
+}
