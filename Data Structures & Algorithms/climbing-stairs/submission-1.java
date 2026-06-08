73fd946e73616b57cc184f06db13404bae568e9c
Rajath Kumar <rok.rajath@gmail.com>
2026-06-08 15:38:29 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <=2 ) {
+            return n;
+        }
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+
+    }
+
+    private int fib(int cur, int n) {
+        if(cur == n || cur == n-1) {
+            return 1;
+        }
+
+        return fib(cur + 1, n) + fib(cur + 2, n);
+    }
+}
